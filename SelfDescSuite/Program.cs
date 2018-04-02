dd97e50bffbf7fa51d612687c45eda87ac8b5910
Moncef Naji <moncef.naji@gmail.com>
2018-04-02 19:40:40 -0400

    Add project files.

---
+++ SelfDescSuite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfDescSuite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
